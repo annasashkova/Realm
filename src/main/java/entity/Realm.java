2e893e67a450cfcaa3b3ee602e9bb46846d3e0bd
annasashkova <avlaada99@gmail.com>
2024-11-19 19:33:55 +0300

    singleton pattern implemented

--- src/main/java/entity/Realm.java
+++ src/main/java/entity/Realm.java
@@ -1,4 +1,16 @@
 package entity;
 
 public class Realm {
+
+    private static final Realm INSTANCE = new Realm();
+
+    private Realm(){
+
+    }
+
+    public static Realm getInstance(){
+        return INSTANCE;
+    }
+
+
 }
