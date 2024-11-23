22f7e14eb3c4635f3b43cf36ac0665513c6f997f
annasashkova <avlaada99@gmail.com>
2024-11-23 14:07:51 +0300

    added constructor, getters and setters for District class

--- src/main/java/entity/District.java
+++ src/main/java/entity/District.java
@@ -1,6 +1,8 @@
 package entity;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class District {
     private ArrayList<City> cities;
@@ -8,13 +10,40 @@ public class District {
     private City regionalCenter;
 
     public District(){
+        int counter = (int)(2+ Math.random()*4);
+        for(int i = 0; i<=counter; i++) {
+            cities.set(i, new City());
+        }
 
+        for(City city : cities){
+            this.square = this.square + city.getSquare();
+        }
+
+        Random random = new SecureRandom();
+        this.regionalCenter = cities.get(random.nextInt(cities.size()));
     }
 
-    public District(ArrayList<City> cities, int square,City regionalCenter){
+    public ArrayList<City> getCities(){
+        return cities;
+    }
+
+    public void setCities(ArrayList<City>cities){
         this.cities = cities;
+    }
+
+    public int getSquare() {
+        return square;
+    }
+
+    public void setSquare(int square) {
         this.square = square;
+    }
+
+    public City getRegionalCenter() {
+        return regionalCenter;
+    }
+
+    public void setRegionalCenter(City regionalCenter) {
         this.regionalCenter = regionalCenter;
     }
-
 }
