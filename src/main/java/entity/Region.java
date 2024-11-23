319a404b0386eee6132cdff3c8e63e4541dd68a4
annasashkova <avlaada99@gmail.com>
2024-11-23 14:11:59 +0300

    added constructor, getters and setters for Region class

--- src/main/java/entity/Region.java
+++ src/main/java/entity/Region.java
@@ -8,11 +8,29 @@ public class Region {
     private int square;
 
     public Region() {
+        int counter = (int)(1+ Math.random()*2);
+        for(int i = 0; i<=counter; i++) {
+            districts.set(i, new District());
+        }
 
+        for(District district : districts){
+            this.square = this.square + district.getSquare();
+        }
     }
 
-    public Region(ArrayList<District> districts, int square) {
+    public ArrayList<District> getDistricts() {
+        return districts;
+    }
+
+    public void setDistricts(ArrayList<District> districts) {
         this.districts = districts;
+    }
+
+    public int getSquare() {
+        return square;
+    }
+
+    public void setSquare(int square) {
         this.square = square;
     }
 }
